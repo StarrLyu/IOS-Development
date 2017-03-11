e386d015a87a00ce6717c8e2ae9d5a4c32c7f12c
吕思达 <lyusida@lvsidadeMacBook-Pro.local>
2017-03-11 20:13:34 +0800

    Initial Commit

---
+++ LovePoem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LovePoem
+//
+//  Created by 吕思达 on 2017/3/11.
+//  Copyright © 2017年 吕思达. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
